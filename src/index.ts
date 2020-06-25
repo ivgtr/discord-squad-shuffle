eca393b70f3cd953fe580d701ca057f577893d58
ivgtr <gear.1112t.o@gmail.com>
2020-06-25 15:45:28 +0900

    🚩 人数を基準にした振り分けを実装

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,28 @@ client.on('message', async (message) => {
   // 自分の発言を無視
   if (message.author.bot) return
 
-  if (message.content.startsWith('!t') && message.guild) {
+  if (
+    (message.content.startsWith('!t') && message.guild) ||
+    (message.content.startsWith('!m') && message.guild)
+  ) {
     if (!message.mentions.members) return
     if (!message.member) return
     if (message.mentions.members.size <= 1) {
-      const teams = Number(message.content.split(' ')[1] || 1)
+      const start = message.content.split(' ')[0] || 'error'
+      const first = Number(message.content.split(' ')[1] || 1)
+      const second = Number(message.content.split(' ')[2] || 1)
+      let pattern: string
+      switch (start) {
+        case '!t':
+          pattern = 'team'
+          break
+        case '!m':
+          pattern = 'member'
+          break
+        default:
+          pattern = 'error'
+          break
+      }
       if (!message.mentions.members.size) {
         const { member } = message
         if (!member.voice.channel) {
@@ -30,39 +47,18 @@ client.on('message', async (message) => {
           return
         }
         const names = member.voice.channel.members.map((m) => m.displayName)
-        shuffle(message, 'team', names, teams, 0)
+        shuffle(message, pattern, names, first, second)
       } else {
         const member = message.mentions.members.first()
         if (!member) return
         if (!member.voice.channel) return
         const names = member.voice.channel.members.map((m) => m.user.tag)
-        shuffle(message, 'team', names, teams, 0)
+        shuffle(message, pattern, names, first, second)
       }
     } else {
       message.channel.send('メンバーは1人を指定してください')
     }
   }
-  // if (!member.voice.channel) {
-  //   channel.send(
-  //     '発言者はボイスチャンネルに参加するかメンションで指定してください'
-  //   )
-  // }
-  // const tags = member.voice.channel.members.map((m) => m.displayName)
-  // message.channel.send(tags.join('\n'))
 })
-
-// client.on('message', async (message) => {
-//   if (message.content.startsWith('!members') && message.guild) {
-//     if (message.mentions.members.size !== 1)
-//       return message.channel.send('メンバーを1人指定してください')
-//     const member = message.mentions.members.first()
-//     if (!member.voice.channel)
-//       return message.channel.send(
-//         '指定したメンバーがボイスチャンネルに参加していません'
-//       )
-//     const tags = member.voice.channel.members.map((member) => member.user.tag)
-//     message.channel.send(tags.join('\n'))
-//   }
-// })
 
 client.login(process.env.DISCORD_TOKEN)
--- src/shuffle.ts
+++ src/shuffle.ts
@@ -5,15 +5,15 @@ export const shuffle = (
   message: Discord.Message,
   type: string,
   names: string[],
-  teams = 1,
+  num = 1,
   members = 1
 ): void => {
   if (type === 'team') {
     const shuffleNames = action(names)
-    const teamLength = Math.floor(names.length / teams)
+    const teamLength = Math.floor(names.length / num)
     const outputText: string[] = []
-    if (!(names.length % teams) && teams > 0) {
-      for (let i = 1; i <= teams; i += 1) {
+    if (!(names.length % num) && num > 0) {
+      for (let i = 1; i <= num; i += 1) {
         outputText.push(`${i}つ目のチーム`)
         for (let n = 1; n <= teamLength; n += 1) {
           const l = shuffleNames.splice(0, 1)[0]
@@ -26,15 +26,19 @@ export const shuffle = (
         embed: {
           color: 16757683,
           author: {
-            name: `${teamLength}人のチームを${teams}チーム作ります`
+            name: `${num}チームを作ります`
           },
           description: outputText.join('\n')
         }
       }
       message.channel.send(embed)
     } else {
-      const spare = shuffleNames.splice(0, 1)[0]
-      for (let i = 1; i <= teams; i += 1) {
+      const spare = names.length - teamLength * num
+      const spareArray: string[] = []
+      for (let i = 1; i <= spare; i += 1) {
+        spareArray.push(shuffleNames.splice(0, 1)[0])
+      }
+      for (let i = 1; i <= num; i += 1) {
         outputText.push(`${i}つ目のチーム`)
         for (let n = 1; n <= teamLength; n += 1) {
           const l = shuffleNames.splice(0, 1)[0]
@@ -44,13 +48,71 @@ export const shuffle = (
       }
 
       outputText.push('余り')
-      outputText.push(spare)
+      for (let i = 0; i < spareArray.length; i += 1) {
+        outputText.push(spareArray[i])
+      }
       outputText.push('')
       const embed = {
         embed: {
           color: 16757683,
           author: {
-            name: `${teamLength}人のチームを${teams}チーム作ります`
+            name: `${teamLength}人のチームを${num}チーム作ります`
+          },
+          description: outputText.join('\n')
+        }
+      }
+      message.channel.send(embed)
+    }
+  } else if (type === 'member') {
+    const shuffleNames = action(names)
+    const teamLength = Math.floor(names.length / num)
+    const outputText: string[] = []
+    if (!(names.length % num) && num > 0) {
+      for (let i = 1; i <= teamLength; i += 1) {
+        outputText.push(`${i}つ目のチーム`)
+        for (let n = 1; n <= num; n += 1) {
+          const l = shuffleNames.splice(0, 1)[0]
+          outputText.push(l)
+        }
+        outputText.push('')
+      }
+
+      const embed = {
+        embed: {
+          color: 16757683,
+          author: {
+            name: `${num}人のチームを${teamLength}チーム作ります`
+          },
+          description: outputText.join('\n')
+        }
+      }
+      message.channel.send(embed)
+    } else {
+      const spare = names.length - teamLength * num
+      const spareArray: string[] = []
+      for (let i = 1; i <= spare; i += 1) {
+        spareArray.push(shuffleNames.splice(0, 1)[0])
+      }
+
+      for (let i = 1; i <= teamLength; i += 1) {
+        outputText.push(`${i}つ目のチーム`)
+        for (let n = 1; n <= num; n += 1) {
+          const l = shuffleNames.splice(0, 1)[0]
+          outputText.push(l)
+        }
+        outputText.push('')
+      }
+
+      outputText.push('余り')
+      for (let i = 0; i < spareArray.length; i += 1) {
+        outputText.push(spareArray[i])
+      }
+      outputText.push('')
+      const embed = {
+        embed: {
+          color: 16757683,
+          author: {
+            name: `${num}人のチームを${teamLength}チーム作ります`
           },
           description: outputText.join('\n')
         }
